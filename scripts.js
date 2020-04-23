600f32af79b686cf4a533cf88d4d8d32ce15d060
dorabehan <64091830+dorabehan@users.noreply.github.com>
2020-04-23 16:45:52 +0200

    insert first part

---
+++ scripts.js
@@ -0,0 +1,29 @@
+let name = 'Dora';
+
+console.log(name + " 's apples");
+console.log('Hello world!');
+
+// This is a javascript comment
+
+/*
+This is a multiline JavaScript comment.
+*/
+[26, 'Dora', true]
+(3) [26, "Dora", true]
+let person = {}
+undefined
+let person = {
+    name: 'Dora',
+    age: 26,
+    oldEnoughToDrive: true
+}
+undefined
+person
+{name: "Dora", age: 26, oldEnoughToDrive: true}
+person.name
+"Dora"
+person.age
+26
+person.age +10
+36
+
